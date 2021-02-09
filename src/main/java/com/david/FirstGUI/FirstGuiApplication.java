97f809192fab6b45ac0a3109c73313e2a76b4df7
David Raysor <69765212+DaveR0629@users.noreply.github.com>
2021-02-09 17:19:21 -0500

    FirstGUI

---
+++ src/main/java/com/david/FirstGUI/FirstGuiApplication.java
@@ -0,0 +1,13 @@
+package com.david.FirstGUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGuiApplication.class, args);
+	}
+
+}
